7ff042be85e76550d2a24abd8fb4049164be43d1
taodtu <taodtu.gmail.com>
2019-09-14 16:23:46 +0100

    install axios and vue-axios plugin in main.ts

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+    import {Vue, Component, Prop} from 'vue-property-decorator';
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
